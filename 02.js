1f4e8eb1ca550ab9f3c173b338f5b16bacf86916
Nina Motorna <nina.motorna@gmail.com>
2021-12-12 23:13:31 +0100

    adds task 02

---
+++ 02.js
@@ -0,0 +1,24 @@
+// Return a new array with the strings filtered out
+
+//v1
+function filter_list(l) {
+  let arr = [];
+  l.forEach((item) => {
+    if (Number.isInteger(item)) {
+      return arr.push(item);
+    }
+  });
+  return arr;
+}
+
+//v2
+function filter_list2(l) {
+  return l.filter((item) => Number.isInteger(item));
+}
+
+// Test.assertSimilar(filter_list([1, 2, "a", "b"]), [1, 2]);
+// Test.assertSimilar(filter_list([1, "a", "b", 0, 15]), [1, 0, 15]);
+// Test.assertSimilar(filter_list([1, 2, "aasf", "1", "123", 123]), [1, 2, 123]);
+
+const a = filter_list([1, "a", "b", 0, 15]);
+console.log(a);
