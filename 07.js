4a00348dc977e8eb3651d015c2f4ea4df8c3c205
Nina Motorna <nina.motorna@gmail.com>
2022-01-07 21:15:05 +0100

    adds task 07

---
+++ 07.js
@@ -0,0 +1,26 @@
+// Simple Pig Latin
+
+/* Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+ */
+
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map((item) => {
+      if (typeof item === "string") {
+        const letters = item.split("");
+        return `${item.slice(1)}${item.slice(0, 1)}ay`;
+      } else {
+        return item;
+      }
+    })
+    .join(" ");
+}
+
+const a = pigIt("Pig latin is cool");
+console.log(a);
